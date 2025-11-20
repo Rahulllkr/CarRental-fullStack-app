c38108216569b8d294afca9ae3156b0d9d31e6c5
Rahulllkr <rahulkumar8604745@gmail.com>
2025-11-21 00:33:42 +0530

    admin dashboard comp.

--- client/src/pages/owner/Dashboard.jsx
+++ client/src/pages/owner/Dashboard.jsx
@@ -3,6 +3,8 @@ import { assets, dummyDashboardData } from '../../assets/assets'
 import Title from '../../components/owner/Title'
 
 const Dashboard = () => {
+
+    const currency = import.meta.env.VITE_CURRENCY
     const [data,setData] = useState({
         totalCars:0,
         totalBookings:0,
@@ -47,11 +49,34 @@ const Dashboard = () => {
             {/* Recent Bookings */}
             <div className='p-4 md:p-6 border border-borderColor rounded-md max-w-lg w-full'>
                 <h1 className='text-lg font-medium'>Recent Booking</h1>
-                <p>Latest customer bookings</p>
+                <p className='text-gray-500'>Latest customer bookings</p>
+                {
+                    data.recentBookings.map((booking,index) => (
+                        <div className='mt-4 flex items-center justify-between ' key={index}>
+                            <div className='flex items-center gap-2'>
+                                <div className='hidden md:flex items-center justify-center w-12 h-12 rounded-full bg-primary/10'>
+                                    <img className='h-5 w-5' src={assets.listIconColored} alt="" />
+                                </div>
+                                <div>
+                                    <p>{booking.car.brand} {booking.car.model}</p>
+                                    <p className='text-sm text-gray-500'>{booking.createdAt.split("T")[0]}</p>
+                                </div>
+                            </div>
+                            <div className='flex items-center gap-2 font-medium'>
+                                <p className='text-sm text-gray-500'>{currency}{booking.price}</p>
+                                <p className='px-3 py-0.5 border border-borderColor rounded-full text-sm'>{booking.status}</p>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
 
             {/* Monthly Revenue */}
-            <div></div>
+            <div className='p-4 md:p-6 mb-6 border border-borderColor rounded-md w-full md:max-w-xs'>
+                <h1 className='text-lg font-medium'>Monthly Revenue</h1>
+                <p className='text-gray-500'>Revenue for current month</p>
+                <p className='text-3xl mt-6 font-semibold text-primary'>{currency}{data.monthlyRevenue}</p>
+            </div>
         </div>
 
     </div>
