6ea48f1b22ccdfdd2161b8cfda4cf838d4163b42
Rahulllkr <rahulkumar8604745@gmail.com>
2025-11-20 23:48:05 +0530

    dashboard

--- client/src/pages/owner/Dashboard.jsx
+++ client/src/pages/owner/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { assets, dummyDashboardData } from '../../assets/assets'
+import Title from '../../components/owner/Title'
 
 const Dashboard = () => {
     const [data,setData] = useState({
@@ -23,7 +24,25 @@ const Dashboard = () => {
     },[])
 
   return (
-    <div></div>
+    <div className='px-4 pt-10 md:px-10 flex-1'>
+        <Title title="Admin Dashboard" subTitle="Monitor overall platform performance including total cars, bookings, revenue, and recent activities" />
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl'>
+            {
+                dashboardCards.map((card,index) => (
+                    <div className='flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor' key={index}>
+                        <div>
+                            <h1 className='text-xs text-gray-500'>{card.title}</h1>
+                            <p className='text-lg font-semibold'>{card.value}</p>
+                        </div>
+
+                        <div className='flex items-center justify-center w-10 h-10 rounded-full bg-primary/10'>
+                            <img className='h-4 w-4' src={card.icon} alt="" />
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
   )
 }
 
