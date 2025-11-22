1512a16ca9090a0627eb8c2970f16e6a694a8cc6
Rahulllkr <rahulkumar8604745@gmail.com>
2025-11-23 00:49:49 +0530

    manage cars done

--- client/src/pages/owner/ManageCars.jsx
+++ client/src/pages/owner/ManageCars.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { dummyCarData } from '../../assets/assets'
+import { assets, dummyCarData } from '../../assets/assets'
 import Title from '../../components/owner/Title'
 
 const ManageCars = () => {
@@ -44,7 +44,13 @@ const ManageCars = () => {
                   <td className='p-3 max-md:hidden'>{car.category}</td>
                   <td className='p-3'>{currency}{car.pricePerDay}/day</td>
                   <td className='p-3 max-md:hidden'>
-                    
+                    <span className={`px-3 py-1 rounded-full text-xs ${car.isAvaliable ? "bg-green-100 text-green-500" : "bg-red-100 text-red-500"}`}>
+                      {car.isAvaliable ? "Available" : "Unavailable"}
+                    </span>
+                  </td>
+                  <td className='flex items-center p-3'>
+                    <img className='cursor-pointer' src={car.isAvaliable ? assets.eye_close_icon : assets.eye_icon} alt="" />
+                    <img className='cursor-pointer' src={assets.delete_icon} alt="" />
                   </td>
                 </tr>
               ))
